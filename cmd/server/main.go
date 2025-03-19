bc8169d25c4832be108959f9f39abb192e75f23e
farhan-helmy <farhanhlmy@gmail.com>
2025-03-19 12:48:22 +0800

    feat: add rate limiter

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -10,6 +10,8 @@ import (
 	"github.com/farhan-helmy/sedekahje-be/internal/db"
 	"github.com/farhan-helmy/sedekahje-be/internal/routes"
 	"github.com/farhan-helmy/sedekahje-be/internal/utils"
+	"github.com/throttled/throttled/v2"
+	"github.com/throttled/throttled/v2/store/memstore"
 )
 
 func main() {
@@ -24,7 +26,27 @@ func main() {
 
 	router.Use(utils.LoggingMiddleware)
 
+	store, err := memstore.NewCtx(65536)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	quota := throttled.RateQuota{
+		MaxRate:  throttled.PerMin(20),
+		MaxBurst: 5,
+	}
+
+	rateLimiter, err := throttled.NewGCRARateLimiterCtx(store, quota)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	httpRateLimiter := throttled.HTTPRateLimiterCtx{
+		RateLimiter: rateLimiter,
+		VaryBy:      &throttled.VaryBy{Path: true},
+	}
+
 	// Start server
 	log.Println("Server running on :3000")
-	log.Fatal(http.ListenAndServe(":3000", router))
+	log.Fatal(http.ListenAndServe(":3000", httpRateLimiter.RateLimit(router)))
 }
